00b7a1d6cf6fc821d54c7b822862c0e76f7abef1
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-10-19 09:39:17 +0700

    Create ex3.c

---
+++ week_4/ex3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+static char *s;
+static int N;
+void STACKinit(int maxN){ 
+	s = malloc(maxN*sizeof(char));
+}
+int STACKempty(){
+	return N == 0; 
+}
+void STACKpush(char item){
+	s[N++] = item; 
+}
+char STACKpop(){
+	return s[--N];
+}
+int main(){
+	char a[] = "3+5*4";
+	int i;
+	N = strlen(a);
+	STACKinit(N);
+	for (i = 0; i < N; i++){
+		if (a[i] == ')') printf("%c ", STACKpop());
+		if ((a[i] == '+') || (a[i] == '*')) STACKpush(a[i]);
+		if ((a[i] >= '0') && (a[i] <= '9')) printf("%c ", a[i]);
+	}
+	printf("\n");
+}
+
