13624d2fd861ec5f47a4c14d0747ca91cb0205c8
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-10-25 12:59:48 +0700

    Update ex4.c

--- week_4/ex4.c
+++ week_4/ex4.c
@@ -1,30 +1,86 @@
-#include<stdio.h>
-#include<stdlib.h>
-static char *s;
-static int N;
-void STACKinit(int maxN){ 
-	s = malloc(maxN*sizeof(char));
-}
-int STACKempty(){
-	return N == 0; 
-}
-void STACKpush(char item){
-	s[N++] = item; 
-}
-char STACKpop(){
-	return s[--N];
-}
-int main(){
-	char a[] = "5 4 + 6 *";
-	int i;
-	N = strlen(a);
-	STACKinit(N);
-	for (i = 0; i < N; i++){
-		if (a[i] == '+') STACKpush(STACKpop()+STACKpop());
-		if (a[i] == '*') STACKpush(STACKpop()*STACKpop());
-		if ((a[i] >= '0') && (a[i] <= '9')) STACKpush(0);
-		while ((a[i] >= '0') && (a[i] <= '9')) STACKpush(10*STACKpop() + (a[i++]-'0'));	
-	}
-	printf("%d \n", STACKpop());
-}
+#include <stdio.h> 
+#include <string.h> 
+#include <ctype.h> 
+#include <stdlib.h> 
+  
+// Stack type 
+struct Stack 
+{ 
+    int top; 
+    unsigned capacity; 
+    int* array; 
+}; 
+  
+struct Stack* createStack( unsigned capacity ) 
+{ 
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack)); 
+  
+    if (!stack) return NULL; 
+  
+    stack->top = -1; 
+    stack->capacity = capacity; 
+    stack->array = (int*) malloc(stack->capacity * sizeof(int)); 
+  
+    if (!stack->array) return NULL; 
+  
+    return stack; 
+} 
+  
+int isEmpty(struct Stack* stack) 
+{ 
+    return stack->top == -1 ; 
+} 
+  
+char peek(struct Stack* stack) 
+{ 
+    return stack->array[stack->top]; 
+} 
+  
+char pop(struct Stack* stack) 
+{ 
+    if (!isEmpty(stack)) 
+        return stack->array[stack->top--] ; 
+    return '$'; 
+} 
+  
+void push(struct Stack* stack, char op) 
+{ 
+    stack->array[++stack->top] = op; 
+} 
+  
+  
+int evaluatePostfix(char* exp) 
+{ 
+    struct Stack* stack = createStack(strlen(exp)); 
+    int i; 
+
+    if (!stack) return -1; 
+  
+    for (i = 0; exp[i]; ++i) 
+    { 
+
+        if (isdigit(exp[i])) 
+            push(stack, exp[i] - '0'); 
+        else
+        { 
+            int val1 = pop(stack); 
+            int val2 = pop(stack); 
+            switch (exp[i]) 
+            { 
+            case '+': push(stack, val2 + val1); break; 
+            case '-': push(stack, val2 - val1); break; 
+            case '*': push(stack, val2 * val1); break; 
+            case '/': push(stack, val2/val1); break; 
+            } 
+        } 
+    } 
+    return pop(stack); 
+} 
+  
+int main() 
+{ 
+    char exp[] = "231*+9-"; 
+    printf ("postfix evaluation: %d", evaluatePostfix(exp)); 
+    return 0; 
+} 
 
