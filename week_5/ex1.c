07617e502edc094e4c566be61bd0529741563bee
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-10-31 16:23:25 +0700

    Update ex1.c

--- week_5/ex1.c
+++ week_5/ex1.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 struct address{
-	char tele[20];
+	char telephone[20];
 	char name[20];
 	char email[20];
 };
@@ -17,60 +17,71 @@ struct queue{
 	node *rear;
 };
 typedef struct queue queue;
-void MakeQueue(queue *Q){
-	node *header;
-	header = (node*)malloc(sizeof(node));
-	header->next = NULL;
-	Q->front = header;
-	Q->rear = header;
+node* newNode(address k){
+	node* temp = (node*)malloc(sizeof(node));
+	temp->data = k;
+	temp->next = NULL;
+	return temp; 
 }
-int EmptyQueue(queue *Q){
-	return Q->front == Q->rear;
+queue* createQueue(){
+	queue* Q = (queue*)malloc(sizeof(queue));
+	Q->front = NULL;
+	Q->rear = NULL;
+	return Q;
 }
-void EnQueue(address X, queue *Q){
-	Q->rear->next = (node*)malloc(sizeof(node));
-	Q->rear=Q->rear->next;
-	Q->rear->data=X;
-	Q->rear->next=NULL;
-}
-address DeQueue(queue *Q){
-	if(!EmptyQueue(Q)){
-		node *T;
-		address temp;
-		T = Q->front;
-		temp = T->data;
-		Q->front = Q->front->next;
-		free(T);
-		return temp;
+void EnQueue(address X, queue* Q){
+	node* temp = newNode(X);
+	if(Q->rear == NULL){
+		Q->front = temp;
+		Q->rear = temp;
+		return ;
 	}
+	Q->rear->next = temp;
+	Q->rear = temp;
+}
+void DeQueue(queue* Q){
+	if(Q->front == NULL){
+		Q->rear = NULL;
+		return ; 
+	}
+	node* temp = Q->front;
+	Q->front = Q->front->next;
+	free(temp);
 }
 int main(){
+	queue* Q = createQueue();
 	FILE *f;
-	f = fopen("new.txt","r");
-	queue *Q;
-	MakeQueue(Q);
+	f = fopen("C:/Users/Administrator/Desktop/new.txt","r");
+	if (f == NULL)
+    {
+    	perror("Khong the mo tep de doc.\n");
+		return 1;
+    }
 	address value;
 	while(!feof(f)){
-		fgets(value.name,255,f);
-		value.name[strlen(value.name)-1] = '\0';
-		printf("%s\n",value.name);
-		fgets(value.tele,255,f);
-		value.tele[strlen(value.tele)-1] = '\0';
-		printf("%s\n",value.tele);
-		fgets(value.email,255,f);
-		value.email[strlen(value.email)-1] = '\0';
+		fscanf(f,"%s\t",value.name);
+		printf("%s\t",value.name);
+		fscanf(f,"%s\t",value.telephone);
+		printf("%s\t",value.telephone);
+		fscanf(f,"%s\n",value.email);
 		printf("%s\n",value.email);
 		EnQueue(value,Q);
 	}
 	fclose(f);
 	FILE *f1;
-	f1 = fopen("test.txt","w");
-	while(!EmptyQueue(Q)){
-		value = DeQueue(Q);
-		fprintf(f1,"%s\n",value.name);
-		fprintf(f1,"%s\n",value.tele);
+	f1 = fopen("C:/Users/Administrator/Desktop/test.txt","w");
+	if (f1 == NULL)
+    {
+    	perror("Khong the mo tep de ghi.\n");
+		return 1;
+    }
+	while(Q->front != NULL){
+		value = Q->front->data;
+		fprintf(f1,"%s\t",value.name);
+		fprintf(f1,"%s\t",value.telephone);
 		fprintf(f1,"%s\n",value.email);
+		DeQueue(Q);
 	}
 	fclose(f1);
 	return 0;
-}
+}	
