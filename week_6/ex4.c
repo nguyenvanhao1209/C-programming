b4f7b93a0ac7363a7925c5f41eaff04caec1fffe
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-11-02 09:15:40 +0700

    Create ex4.c

---
+++ week_6/ex4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int Bsearch(int A[],int start,int finish,int b){
+	int middle = (start + finish)/2;
+	if(start == finish){
+		if(A[middle]==b) return middle;
+		else return -1;
+	}
+	if(b==A[middle]){
+		return middle;
+	}
+	if(A[middle]<b){
+		return Bsearch(A,middle+1,finish,b);
+	}
+	else Bsearch(A,start,middle-1,b);
+}
+int main(){
+	int a[10];
+	int i;
+	for(i=0;i<10;i++){
+		scanf("%d",&a[i]);
+	}
+	int b;
+	scanf("%d",&b);
+	printf("%d",Bsearch(a,0,9,10));
+	return 0;
+}
