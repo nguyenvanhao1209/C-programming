be1c9a92f9ddd1ddcfa369d4e54f810cceeb9752
nguyenvanhao1209 <88066192+nguyenvanhao1209@users.noreply.github.com>
2021-11-19 14:31:12 +0700

    Create ex4.c

---
+++ week_8/ex4.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAX 20
+typedef struct phoneaddress_t {
+  char name[20];
+  char tel[11];
+  char email[25];
+}phoneaddress;
+typedef struct Node{
+  phoneaddress key;
+  struct Node* Left,Right;
+}NodeType;
+typedef Node* TreeType;
+TreeType Search(char* email,TreeType Root){
+  if (Root == NULL) return NULL;
+  if (strcmp((Root->Key).email, email) == 0) return Root;
+  else if (strcmp((Root->Key).email, email) < 0)
+    return Search(email,Root->right);
+  else {
+    return Search(email,Root->left);
+  }
+}
+void InsertNode(phoneaddress x,TreeType *Root ){
+  if (*Root == NULL){
+    *Root=(NodeType*)malloc(sizeof(NodeType));
+    (*Root)->Key = x;
+    (*Root)->left = NULL;
+    (*Root)->right = NULL;
+  }
+  else if (strcmp(((*Root)->Key).email, x.email) > 0)
+    InsertNode(x, (*Root)->left);
+  else if (strcmp(((*Root)->Key).email, x.email) > 0) 
+    InsertNode(x,(*Root)->right); 
+}
+void printTree(TreeType Root){
+  if(Root != NULL){
+    printTree(Root->left);
+    printf("%-20s\t%-20s\t%-20s\n",Root->key.email,Root->key.name,Root->key.tel);
+    printTree(Root->right);
+  }
+}
+int main(){
+  FILE *fp;
+  phoneaddress phonearr[MAX];
+  TreeType root;
+  int i,n, irc;
+  int n=10;
+  if((fp = fopen("phonebook.dat","rb")) == NULL){
+    printf("Can not open %s.\n", "phonebook.dat");
+  }
+  irc = fread(phonearr, sizeof(phoneaddress), n, fp);
+  fclose(fp);
+  for (i=0; i<n; i++)
+    root = InsertNode(phonearr[i],root);
+  printTree(root);
+  char s[25];
+  fflush(stdin);
+  gets(s);
+  TreeType p = Search(s,root);
+  printf("%-20s\t%-20s\t%-20s\n",p->key.email,p->key.name,p->key.tel);
+}
